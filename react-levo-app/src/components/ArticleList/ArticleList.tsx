fc1620554946ded17686efea43e2fe94b85e9ffc
Shilpe Saxena <shilpesaxena@Shilpes-Laptop.local>
2024-02-17 21:21:34 +0530

    fix: refactor code

--- react-levo-app/src/components/ArticleList/ArticleList.tsx
+++ react-levo-app/src/components/ArticleList/ArticleList.tsx
@@ -4,10 +4,11 @@ import { ArticleComponent } from '../ArticleComponent';
 import { LoadMoreButton } from '../LoadMoreButton';
 import { Spinner } from '../Spinner';
 
-export const ArticleList = ( ) => {
+export const ArticleList = () => {
   const [articles, setArticles] = useState<Article[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
   const [displayedArticles, setDisplayedArticles] = useState<number>(6);
+
   useEffect(() => {
     const fetchData = async () => {
       const articlesData = await fetchArticles();
@@ -16,8 +17,7 @@ export const ArticleList = ( ) => {
     };
     fetchData();
   }, []);
-  console.log("articles",articles);
-  
+
   const handleLoadMore = () => {
     setDisplayedArticles(displayedArticles + 6);
   };
@@ -25,23 +25,23 @@ export const ArticleList = ( ) => {
     <div className="container mx-auto">
       <h1 className="text-3xl font-bold text-center mt-8 mb-4">Spaceflight News Articles</h1>
       {loading ? (
-        <Spinner/>
+        <Spinner />
       ) : (
         <>
-        <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-12">
-          {Array.isArray(articles) && articles?.slice(0, displayedArticles)?.map((article) => (
-            <ArticleComponent
-            key={article.id}
-            title={article.title}
-            summary={article.summary}
-            url={article.url}
-            publishedAt={article.published_at}
-            imageUrl={article?.image_url && article?.image_url || ''}
-          />
-          ))}
-           
-        </ul>
-        {articles.length > displayedArticles && (
+          <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-12">
+            {Array.isArray(articles) && articles?.slice(0, displayedArticles)?.map((article) => (
+              <ArticleComponent
+                key={article.id}
+                title={article.title}
+                summary={article.summary}
+                url={article.url}
+                publishedAt={article.published_at}
+                imageUrl={article?.image_url && article?.image_url || ''}
+              />
+            ))}
+
+          </ul>
+          {articles.length > displayedArticles && (
             <div className="flex justify-center pb-8">
               <LoadMoreButton onClick={handleLoadMore} />
             </div>
